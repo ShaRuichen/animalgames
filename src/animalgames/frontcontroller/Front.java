bcbae8a7902660d670f309b7dfd62406b4308126
leyingzhang <57288034+leyingzhang@users.noreply.github.com>
2020-11-23 21:29:52 +0800

    替边策同学上传

---
+++ src/animalgames/frontcontroller/Front.java
@@ -0,0 +1,62 @@
+public class Front {
+}
+class HomeView {
+    public void show(){
+        System.out.println("Displaying Home Page");
+    }
+}
+class StudentView {
+    public void show(){
+        System.out.println("Displaying Animals Page");
+    }
+}
+class Dispatcher {
+    private StudentView studentView;
+    private HomeView homeView;
+    public Dispatcher(){
+        studentView = new StudentView();
+        homeView = new HomeView();
+    }
+
+    public void dispatch(String request){
+        if(request.equalsIgnoreCase("ANIMALS")){
+            studentView.show();
+        }else{
+            homeView.show();
+        }
+    }
+}
+class FrontController {
+
+    private Dispatcher dispatcher;
+
+    public FrontController(){
+        dispatcher = new Dispatcher();
+    }
+
+    private boolean isAuthenticUser(){
+        System.out.println("User is authenticated successfully.");
+        return true;
+    }
+
+    private void trackRequest(String request){
+        System.out.println("Page requested: " + request);
+    }
+
+    public void dispatchRequest(String request){
+        //记录每一个请求
+        trackRequest(request);
+        //对用户进行身份验证
+        if(isAuthenticUser()){
+            dispatcher.dispatch(request);
+        }
+    }
+}
+class FrontControllerPatternDemo {
+    public static void main(String[] args) {
+        FrontController frontController = new FrontController();
+        frontController.dispatchRequest("HOME");
+        frontController.dispatchRequest("ANIMALS");
+    }
+}
+
