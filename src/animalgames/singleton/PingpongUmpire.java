072890c7515731d9ecbd80dee35b0abbc4b2acc5
Z1026 <49972501+Z1026@users.noreply.github.com>
2020-11-23 16:46:44 +0800

    创建乒乓球裁判

---
+++ src/animalgames/singleton/PingpongUmpire.java
@@ -0,0 +1,18 @@
+package Singleton;
+
+public class PingpongUmpire {
+    //创建 PingpongUmpire 的一个对象
+    private static PingpongUmpire instance = new PingpongUmpire();
+
+    //让构造函数为 private，这样该类就不会被实例化
+    private PingpongUmpire(){}
+
+    //获取唯一可用的对象
+    public static PingpongUmpire getInstance(){
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("已创建乒乓球裁判");
+    }
+}
