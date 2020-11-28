39b00fc592474ee0ff41d5448d31cf2d5116fb14
RayJhin <49810000+Ray1852519@users.noreply.github.com>
2020-11-28 12:35:37 +0800

    Update Show.java

--- src/animalgames/bridge/Show.java
+++ src/animalgames/bridge/Show.java
@@ -4,6 +4,6 @@ public abstract class Show {
     protected SelectAPI selectAPI;
     protected Show(SelectAPI selectAPI){
         this.selectAPI = selectAPI;
-    }
+    }//展示可选时装
     public abstract void select();
 }
