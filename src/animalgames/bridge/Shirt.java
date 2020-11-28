518a99abdbe230ef14ffee65462fecb68a43e62a
RayJhin <49810000+Ray1852519@users.noreply.github.com>
2020-11-28 12:34:21 +0800

    Update Shirt.java

--- src/animalgames/bridge/Shirt.java
+++ src/animalgames/bridge/Shirt.java
@@ -4,9 +4,9 @@ public class Shirt extends Show {
 
     public Shirt(SelectAPI selectAPI) {
         super(selectAPI);
-    }
+    }//展示可选时装
 
     public void select() {
         selectAPI.selectClothes();
-    }
+    }//列举可选时装明细
 }
