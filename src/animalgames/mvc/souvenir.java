78aec46cecaaa9fa07991bca69a8a28a2e75cdfc
Z1026 <49972501+Z1026@users.noreply.github.com>
2020-11-28 21:12:39 +0800

    Update and rename Animal.java to souvenir.java

--- src/animalgames/mvc/souvenir.java
+++ src/animalgames/mvc/souvenir.java
@@ -1,22 +1,22 @@
 package animalgames.mvc;
 
-public class Animal {
+public class souvenir {
 	private int id;
 	private String name;
 	
-	// 获取动物ID
+	// 获取纪念品ID
 	public int getId() {
 		return id;
 	}
-	// 设置动物ID
+	// 设置纪念品ID
 	public void setId(int id) {
 		this.id = id;
 	}
-	// 获取动物名
+	// 获取纪念品名
 	public String getName() {
 		return name;
 	}
-	// 设置动物名
+	// 设置纪念品名
 	public void setName(String name) {
 		this.name = name;
 	}
