73097d5a6b46f791618bfc5f3fe9cfb4a2c8df81
RayJhin <49810000+Ray1852519@users.noreply.github.com>
2020-11-28 12:40:07 +0800

    Update Memento.java

--- src/animalgames/memento/Memento.java
+++ src/animalgames/memento/Memento.java
@@ -5,9 +5,9 @@ public class Memento {
 
     public Memento(String state){
         this.state = state;
-    }
+    }//记录状态
 
     public String getState(){
         return state;
-    }
+    }//返回所需状态
 }
