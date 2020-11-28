3247a6fa4ef4c5ce208114b0b2dbf68124ee686e
Z1026 <49972501+Z1026@users.noreply.github.com>
2020-11-28 17:09:04 +0800

    Update and rename Player.java to game_name.java

--- src/animalgames/iterator/game_name.java
+++ src/animalgames/iterator/game_name.java
@@ -1,7 +1,7 @@
 package animalgames.iterator;
 //创建实现了接口的实体类
-public class Player implements Iterable{
-    public String names[] = {"Robert" , "John" ,"Julie" , "Lora"};
+public class game_name implements Iterable{
+    public String names[] = {"足球" , "篮球" ,"游泳" , "乒乓球"};
 
     @Override
     public Iterator getIterator() {
