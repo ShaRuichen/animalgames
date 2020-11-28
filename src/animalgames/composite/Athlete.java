5c51ffec0231df3f56a8cf772e66047f72bd32e2
RayJhin <49810000+Ray1852519@users.noreply.github.com>
2020-11-28 12:37:55 +0800

    Update Athlete.java

--- src/animalgames/composite/Athlete.java
+++ src/animalgames/composite/Athlete.java
@@ -19,19 +19,19 @@ public class Athlete {
 
     public void add(Athlete e) {
         subordinates.add(e);
-    }
+    }//增添运动员
 
     public void remove(Athlete e) {
         subordinates.remove(e);
-    }
+    }//移动运动员
 
     public List<Athlete> getSubordinates(){
         return subordinates;
-    }
+    }//运动员名单
 
     public String toString(){
         return ("运动员 :[ 名字 : "+ name
                 +", 职位 : "+ dept + ", 编号 :"
                 + number +" ]");
-    }
+    }//输出
 }
