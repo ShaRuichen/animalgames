81e607c0cd2e15e21d98a6fcb1cb5f1baf0ebc0f
Owarimasu <401205991@qq.com>
2020-11-28 13:47:32 +0800

    alert

--- src/animalgames/prototype/PrototypeTest.java
+++ src/animalgames/prototype/PrototypeTest.java
@@ -10,13 +10,13 @@ public class PrototypeTest {
         //clone语句 根据卖票数audienceNum
         //这里需要结合卖票数输出
         Audience cloneAudience1 = (Audience) AudienceCache.getAudience("1");
-        System.out.println(cloneAudience1.getType() + ":" + audienceNum1);
+        System.out.println(cloneAudience1.getType());
 
         Audience cloneAudience2 = (Audience) AudienceCache.getAudience("2");
-        System.out.println(cloneAudience2.getType() + ":" + audienceNum2);
+        System.out.println(cloneAudience2.getType());
 
         Audience cloneAudience3 = (Audience) AudienceCache.getAudience("3");
-        System.out.println(cloneAudience3.getType() + ":" + audienceNum3);
+        System.out.println(cloneAudience3.getType());
 
     }
 }
